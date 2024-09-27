33a6381eaa81a87faba798f81531722fdd412a98
Thomas Antonakis <thomas.antonakis@gmail.com>
2024-09-27 16:57:09 +0300

    Add More Buttons on Main Menu Scene

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -3,19 +3,29 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
-    public void PlayHotSeatGame()
-    {
-        SceneManager.LoadScene("Room"); // Replace with your game scene name
-    }
-
     public void PlaySinglePlayerGame()
     {
         Debug.Log("Starting Single Player Game!");
     }
 
+    public void PlayHotSeatGame()
+    {
+        SceneManager.LoadScene("Room"); // Replace with your game scene name
+    }
+
     public void PlayMultiPlayerGame()
     {
         Debug.Log("Starting Multi Player Game!");
+    }
+
+    public void PlayCampaignMode()
+    {
+        Debug.Log("Starting Campaign Mode!");
+    }
+
+    public void OpenRules()
+    {
+        Debug.Log("Rules Opened!");
     }
 
     public void OpenShop()
